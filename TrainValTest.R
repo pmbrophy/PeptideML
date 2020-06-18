4c862b19bbdea2190ef1df0bc43f8a774dec2b52
unknown <unknown@USAMIL-R90WW32X.corp.waters.com>
2020-06-18 17:30:37 -0400

    Generalized to deal with tensors with 3D tensors with different dimensionality along each axis.

--- TrainValTest.R
+++ TrainValTest.R
@@ -26,31 +26,32 @@ split_trainValTest <- function(sequenceArray, fragArray, frac_test = 0.1, frac_t
   test_x <- sequenceArray[testLocs,,]
   test_y <- fragArray[testLocs,,]
   test_x_dims <- dim(test_x)
-  test_x <- keras::array_reshape(x = test_x, dim = c(test_x_dims[1], test_x_dims[2], 1))
+  test_x <- keras::array_reshape(x = test_x, dim = c(test_x_dims[1], test_x_dims[2], test_x_dims[3]))
   
   #Remove test samples from original data structure
   sequenceArray <- sequenceArray[-testLocs,,]
   fragArray <- fragArray[-testLocs,,]
   
+  sequenceArray_dims <- dim(sequenceArray)
+  fragArray_dim <- dim(fragArray)
+  sequenceArray <- keras::array_reshape(x = sequenceArray, dim = c(sequenceArray_dims[1], sequenceArray_dims[2], test_x_dims[3]))
   
   ###Train/Validate
   #Split remaining into train/validate
-  sequenceArray_n <- nrow(sequenceArray)
-  fragArray_n <- nrow(fragArray)
-  array_i <- c(1:sequenceArray_n)
-  trainLocs <- sample(x = array_i, size = floor(sequenceArray_n * frac_train), replace = FALSE)
+  array_i <- c(1:sequenceArray_dims[1])
+  trainLocs <- sample(x = array_i, size = floor(sequenceArray_dims[1] * frac_train), replace = FALSE)
   
   #Train
-  train_x <- sequenceArray[trainLocs,]
+  train_x <- sequenceArray[trainLocs,,]
   train_y <- fragArray[trainLocs,,]
   train_x_dims <- dim(train_x)
-  train_x <- keras::array_reshape(x = train_x, dim = c(train_x_dims[1], train_x_dims[2], 1))
+  train_x <- keras::array_reshape(x = train_x, dim = c(train_x_dims[1], train_x_dims[2], sequenceArray_dims[3]))
   
   #Validate
-  val_x <- sequenceArray[-trainLocs,]
+  val_x <- sequenceArray[-trainLocs,,]
   val_y <- fragArray[-trainLocs,,]
   val_x_dims <- dim(val_x)
-  val_x <- keras::array_reshape(x = val_x, dim = c(val_x_dims[1], val_x_dims[2], 1))
+  val_x <- keras::array_reshape(x = val_x, dim = c(val_x_dims[1], val_x_dims[2], sequenceArray_dims[3]))
   
   list(train = list(x = train_x, y = train_y),
        validate = list(x = val_x, y = val_y),
